91bc8f46aa710bcbe80b499d7ad4c7e46ebe2a57
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-11-24 10:38:18 +0100

    Added dragon movement
    Added Dragon movement and controls.

--- Game/js/game.js
+++ Game/js/game.js
@@ -5,7 +5,7 @@ var config = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 300 },
+			gravity: { y: 0 },
 			debug: false
 		}
 	},
@@ -33,6 +33,7 @@ function preload() {
 	this.load.image('star', 'assets/star.png');
 	this.load.image('bomb', 'assets/bomb.png');
 	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+	this.load.spritesheet('dragon','./assets/dragon.png', {frameWidth: 144, frameHeight: 125});
 }
 
 function create() {
@@ -47,37 +48,51 @@ function create() {
 	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
 	//  Now let's create some ledges
-	platforms.create(600, 400, 'ground');
-	platforms.create(50, 250, 'ground');
-	platforms.create(750, 220, 'ground');
+	//platforms.create(600, 400, 'ground');
+	//platforms.create(50, 250, 'ground');
+	//platforms.create(750, 220, 'ground');
 
 	// The player and its settings
-	player = this.physics.add.sprite(100, 450, 'dude');
+	player = this.physics.add.sprite(100, 450, 'dragon');
 
 	//  Player physics properties. Give the little guy a slight bounce.
 	player.setBounce(0.2);
 	player.setCollideWorldBounds(true);
 
 	//  Our player animations, turning, walking left and walking right.
+	/*
 	this.anims.create({
 		key: 'left',
-		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+		frames: this.anims.generateFrameNumbers('dragon', { start: 0, end: 3 }),
 		frameRate: 10,
 		repeat: -1
 	});
-
+	*/
+	
+	/*
 	this.anims.create({
 		key: 'turn',
-		frames: [{ key: 'dude', frame: 4 }],
+		frames: [{ key: 'dragon', frame: 4 }],
 		frameRate: 20
 	});
+	*/
+	
+	this.anims.create({
+		key: 'turn',
+		frames: this.anims.generateFrameNumbers('dragon', { start: 0, end: 2 }),
+		frameRate: 7,
+		repeat: -1
+	});
+	player.anims.play('turn');
 
+	/*
 	this.anims.create({
 		key: 'right',
-		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+		frames: this.anims.generateFrameNumbers('dragon', { start: 5, end: 8 }),
 		frameRate: 10,
 		repeat: -1
 	});
+	*/
 
 	//  Input Events
 	cursors = this.input.keyboard.createCursorKeys();
@@ -110,28 +125,46 @@ function create() {
 	this.physics.add.overlap(player, stars, collectStar, null, this);
 
 	this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 }
 
-function update() {
+var deltaTime = 0;
+
+function getForwardVector(sprite)
+{
+	const angleInRadians = Phaser.Math.DegToRad(sprite.angle);
+    const forwardVector = new Phaser.Math.Vector2(Math.cos(angleInRadians), Math.sin(angleInRadians));
+	return forwardVector;
+}
+
+function update(time, delta) {
+	
+	//console.log(getForwardVector(player));
+	console.log(delta);
 	if (gameOver) {
 		return;
 	}
+	
+	//player.anims.play('left', true);
+	
+	let playerVelocity = 10 * delta;
+	let playerTurnSpeed = 0.1;
+	playerForwardVector = getForwardVector(player);
+	player.setVelocityX(playerForwardVector.y*playerVelocity);
+	player.setVelocityY(-playerForwardVector.x*playerVelocity);
 
 	if (cursors.left.isDown) {
-		player.setVelocityX(-160);
-
-		player.anims.play('left', true);
+		//player.setVelocityX(-160);
+		player.angle-=playerTurnSpeed * delta;
 	}
 	else if (cursors.right.isDown) {
-		player.setVelocityX(160);
-
-		player.anims.play('right', true);
+		//player.setVelocityX(160);
+		player.angle+=playerTurnSpeed * delta;
 	}
+	/*
 	else {
 		player.setVelocityX(0);
-
-		player.anims.play('turn');
-	}
+	}*/
 
 	if (cursors.up.isDown && player.body.touching.down) {
 		player.setVelocityY(-330);
