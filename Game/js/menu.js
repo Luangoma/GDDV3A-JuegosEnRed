5ea7b9f5f71a39546fd5c5ec6c2004892fa46860
Sandro808 <ja.vazquez.2020@alumnos.urjc.es>
2023-11-29 21:57:27 +0100

    Añadida interacción con los botones del menú inicial
    Se ha añadido en la función MainMenuCreate (menu.js) la interacción con los botones Jugar, Salir, Ajustes y Créditos.

--- Game/js/menu.js
+++ Game/js/menu.js
@@ -7,6 +7,7 @@ var MainMenu = {
 
 function MainMenuPreload()
 {
+	//Precarga imagen background y botones svg.
 	this.load.svg('botonJugar', 'assets/botonJugar.svg');
 	this.load.svg('botonSalir', 'assets/botonSalir.svg');
 	this.load.svg('botonAjustes', 'assets/botonAjustes.svg');
@@ -17,12 +18,35 @@ function MainMenuPreload()
 
 function MainMenuCreate()
 {
+	//Creación imagen background y botones svg.
 	var menuBackground = this.add.image(0,0, 'menuBackground').setOrigin(0,0);
-	var botonJugar = this.add.image(100, 100, 'botonJugar').setScale(0.5);
-	var botonSalir = this.add.image(100, 160, 'botonSalir').setScale(0.5);
-	var botonAjustes = this.add.image(100, 220, 'botonAjustes').setScale(0.5);
-	var botonCreditos = this.add.image(100, 280, 'botonCreditos').setScale(0.5);
+	var botonJugar = this.add.image(400, 100, 'botonJugar').setScale(0.5);
+	var botonSalir = this.add.image(400, 160, 'botonSalir').setScale(0.5);
+	var botonAjustes = this.add.image(400, 220, 'botonAjustes').setScale(0.5);
+	var botonCreditos = this.add.image(400, 280, 'botonCreditos').setScale(0.5);
+
+	//Hacer los botones interactivos.
+	botonJugar.setInteractive();
+	botonCreditos.setInteractive();
+	botonSalir.setInteractive();
+	botonAjustes.setInteractive();
+
+	//Interacción con los botones.
+	botonJugar.on('pointerdown', function(pointer){
+		console.log("Botón jugar pulsado");
+	});
+	botonCreditos.on('pointerdown', function(pointer){
+		console.log("Botón créditos pulsado");
+	});
+	botonAjustes.on('pointerdown', function(pointer){
+		console.log("Botón ajustes pulsado");
+	});
+	botonSalir.on('pointerdown', function(pointer){
+		console.log("Botón salir pulsado");
+	});
 }
 
 function MainMenuUpdate(time, delta)
-{}
+{
+	
+}
