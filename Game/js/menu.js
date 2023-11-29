1e1bbaee9ed3d728b78cb38ca31afafbac82d7f5
Sandro808 <ja.vazquez.2020@alumnos.urjc.es>
2023-11-29 21:05:28 +0100

    Incluidos assets menú inicial
    Incluidos los assets del background del menú inicial y los botones del menú. Incluidos en el preload y en el create.

--- Game/js/menu.js
+++ Game/js/menu.js
@@ -7,12 +7,21 @@ var MainMenu = {
 
 function MainMenuPreload()
 {
-	this.load.image("menu_background","./assets/menu_background.png");
+	this.load.svg('botonJugar', 'assets/botonJugar.svg');
+	this.load.svg('botonSalir', 'assets/botonSalir.svg');
+	this.load.svg('botonAjustes', 'assets/botonAjustes.svg');
+	this.load.svg('botonCreditos', 'assets/botonCreditos.svg');
+	this.load.image('menuBackground', 'assets/menu_background.png');
+	
 }
 
 function MainMenuCreate()
 {
-	this.add.image(0,0,"menu_background").setOrigin(0,0);
+	var menuBackground = this.add.image(0,0, 'menuBackground').setOrigin(0,0);
+	var botonJugar = this.add.image(100, 100, 'botonJugar').setScale(0.5);
+	var botonSalir = this.add.image(100, 160, 'botonSalir').setScale(0.5);
+	var botonAjustes = this.add.image(100, 220, 'botonAjustes').setScale(0.5);
+	var botonCreditos = this.add.image(100, 280, 'botonCreditos').setScale(0.5);
 }
 
 function MainMenuUpdate(time, delta)
