400b2b94cbf3ac9346bdaa67b6733cf2f68e7958
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-11-30 18:54:18 +0100

    Placeholder house sprites and world tile data
    Added some placeholder house destruction sprites.
    
    Modified the tile class to contain data about it's health and destructibility to determine whether it is something that the players can destroy or not.

--- Game/js/map_test1.js
+++ Game/js/map_test1.js
@@ -34,7 +34,7 @@ function map_test_1_create() {
 	
 	//spawnHouses(this, map_test_1_variables.houses, 100, {x: 0, y: 0}, {x: 2048, y: 2048});
 	
-	//createTiles(this, map_test_1_variables.tiles);
+	createTiles(this, map_test_1_variables.tiles);
 	
 	map_test_1_variables.player1 = new Dragon(this, 0, 1024, 1024);
 	map_test_1_variables.player1.create();
@@ -47,8 +47,9 @@ function map_test_1_create() {
 	this.cameras.main.startFollow(map_test_1_variables.player1.sprite, true);
 	this.cameras.main.setZoom(1);
 	
-	this.cameras.add(0,0,config.width,config.height/2).startFollow(map_test_1_variables.player1.sprite,true).setBounds(0,0,world_width, world_height);
-	this.cameras.add(0,config.height/2,config.width,config.height/2).startFollow(map_test_1_variables.player2.sprite,true).setBounds(0,0,world_width, world_height);
+	//enable again to get the split screen camera
+	//this.cameras.add(0,0,config.width,config.height/2).startFollow(map_test_1_variables.player1.sprite,true).setBounds(0,0,world_width, world_height);
+	//this.cameras.add(0,config.height/2,config.width,config.height/2).startFollow(map_test_1_variables.player2.sprite,true).setBounds(0,0,world_width, world_height);
 }
 
 function map_test_1_update(time, delta) {
