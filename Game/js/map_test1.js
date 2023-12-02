3496deb2eb92cd18816232a1e8a0ba0932c7c488
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-12-02 12:33:51 +0100

    Converted UI scene to a class
    Converted the UI class into a class to better work with Phaser styled scenes.
    
    modified player variables to be global so that they can be accessed from the UI.

--- Game/js/map_test1.js
+++ Game/js/map_test1.js
@@ -1,7 +1,8 @@
+player1 = {};
+player2 = {};
+
 class map_test_1 extends Phaser.Scene
 {
-	player1 = {};
-	player2 = {};
 	flames = {};
 	houses = [];
 	tiles = [];
@@ -26,11 +27,11 @@ class map_test_1 extends Phaser.Scene
 		createTiles(this, this.tiles, 'grass_tile_1');
 		createHouses(this, this.houses, this.tiles, 60, this.flames);
 		
-		this.player1 = new Dragon(this, 0, 1024, 1024, this.flames);
-		this.player2 = new Dragon(this, 1, 800, 800, this.flames);
+		player1 = new Dragon(this, 0, 1024, 1024, this.flames);
+		player2 = new Dragon(this, 1, 800, 800, this.flames);
 
-		addMainCamera(this, this.player1);
-		addSplitScreenCamera(this, this.player1, this.player2, 0);
+		addMainCamera(this, player1);
+		addSplitScreenCamera(this, player1, player2, 1);
 
 		// Se hace launch para que la escena UI corra de forma simultánea a esta escena (map1).
 		// Si se hace launch en game.js no funciona.
@@ -39,8 +40,8 @@ class map_test_1 extends Phaser.Scene
 	
 	update(time, delta)
 	{
-		this.player1.update(time, delta);
-		this.player2.update(time, delta);
+		player1.update(time, delta);
+		player2.update(time, delta);
 		
 		
 		for(let i = 0; i < this.houses.length; ++i)
@@ -48,6 +49,20 @@ class map_test_1 extends Phaser.Scene
 			this.houses[i].update(time, delta);
 		}
 		
-		console.log("p1: " + this.player1.points + ", p2: " + this.player2.points);
+		console.log("p1: " + player1.points + ", p2: " + player2.points);
+	}
+	
+	shutdown()
+	{
+		super.shutdown();
+	}
+	
+	destroy()
+	{
+		player1 = null;
+		player2 = null;
+		houses = [];
+		tiles = [];
+		super.destroy();
 	}
 };
--- Game/js/ui.js
+++ Game/js/ui.js
@@ -1,77 +1,75 @@
-var ui = {
-	key: 'ui',
-	preload: UIPreload,
-	create: UICreate,
-	update: UIUpdate
-};
+class ui extends Phaser.Scene
+{
+	textoTemporizador = {};
+	tiempoInicial = {};
+	temporizadorPartida = {};
 
-//Variables temporizador partida
-var textoTemporizador;
-var tiempoInicial;
-var temporizadorPartida;
+	preload()
+	{
 
-function UIPreload(){
-
-	this.load.svg('dragon1graphic', 'assets/dragon1graphic.svg');
-	this.load.svg('dragon2graphic', 'assets/dragon2graphic.svg');
-
-}
-
-function UICreate(){
-    
-    // TEMPORIZADOR PARTIDA
-
-	// Tiempo en segundos de la cuenta atrás.
-	this.tiempoInicial = 80;
-	
-	// Texto de la interfaz donde aparece el tiempo restante. Llama a formatoTiempo() para poner el tiempo
-	// en minutos y segundos.
-	this.textoTemporizador = this.add.text(32,32, 'Tiempo restante: ' + formatoTiempo(this.tiempoInicial));
-
-    this.textoTemporizador.setScrollFactor(0);
-
-	//Cada 1000 ms, es decir, 1 segundo se llama a la función temporizadorTerminado. Para ello se hace en bucle.
-	this.temporizadorPartida = this.time.addEvent({ delay: 1000, callback: temporizadorTerminado, callbackScope: this, loop: true });
-
-
-	
-	// GRÁFICOS DE LOS DRAGONES
-
-	var graficoDragon1 = this.add.image(40, 450, 'dragon1graphic').setOrigin(0,0);
-	graficoDragon1.setScale(0.4);
-	var graficoDragon2 = this.add.image(650, 450, 'dragon2graphic').setOrigin(0,0);
-	graficoDragon2.setScale(0.4);
-}
-
-function UIUpdate(time, delta){
-
-}
-
-// Esta función es llamada cada 1 segundo para actualizar el contador de tiempo restante.
-function temporizadorTerminado() {
-	// Se resta 1 segundo al tiempo inicial.
-	this.tiempoInicial = this.tiempoInicial - 1;
-	// Se actualiza el texto del tiempo restante.
-	this.textoTemporizador.setText('Tiempo restante: ' + formatoTiempo(this.tiempoInicial));
-
-	// Si el tiempo inicial es 0, la partida ha terminado.
-	if (this.tiempoInicial === 0){
-		console.log("GAME OVER");
-
-		// Ir a la escena GAME OVER.
+		this.load.svg('dragon1graphic', 'assets/dragon1graphic.svg');
+		this.load.svg('dragon2graphic', 'assets/dragon2graphic.svg');
 	}
-}
+
+	create()
+	{
+		
+		// TEMPORIZADOR PARTIDA
+
+		// Tiempo en segundos de la cuenta atrás.
+		this.tiempoInicial = 80;
+		
+		// Texto de la interfaz donde aparece el tiempo restante. Llama a formatoTiempo() para poner el tiempo
+		// en minutos y segundos.
+		this.textoTemporizador = this.add.text(32,32, 'Tiempo restante: ' + this.formatoTiempo(this.tiempoInicial));
+
+		this.textoTemporizador.setScrollFactor(0);
+
+		//Cada 1000 ms, es decir, 1 segundo se llama a la función temporizadorTerminado. Para ello se hace en bucle.
+		this.temporizadorPartida = this.time.addEvent({ delay: 1000, callback: this.temporizadorTerminado, callbackScope: this, loop: true });
+
+
+		
+		// GRÁFICOS DE LOS DRAGONES
+
+		var graficoDragon1 = this.add.image(40, 450, 'dragon1graphic').setOrigin(0,0);
+		graficoDragon1.setScale(0.4);
+		var graficoDragon2 = this.add.image(650, 450, 'dragon2graphic').setOrigin(0,0);
+		graficoDragon2.setScale(0.4);
+	}
+
+	update(time, delta)
+	{
+
+	}
+	
+	// Esta función es llamada cada 1 segundo para actualizar el contador de tiempo restante.
+	temporizadorTerminado() {
+		// Se resta 1 segundo al tiempo inicial.
+		this.tiempoInicial = this.tiempoInicial - 1;
+		// Se actualiza el texto del tiempo restante.
+		this.textoTemporizador.setText('Tiempo restante: ' + this.formatoTiempo(this.tiempoInicial));
+
+		// Si el tiempo inicial es 0, la partida ha terminado.
+		if (this.tiempoInicial === 0){
+			console.log("GAME OVER");
+
+			// Ir a la escena GAME OVER.
+		}
+	}
 
 
 
-//Esta función coge segundos y lo formatea en minutos y segundos (por ejemplo, 80 -> 1:20)
-function formatoTiempo(segundos) {
-	// Minutos
-    var minutos = Math.floor(segundos/60);
-    // Segundos
-    var parteEnSegundos = segundos%60;
-    // Añadir ceros a la izquierda a los segundos
-    parteEnSegundos = parteEnSegundos.toString().padStart(2,'0');
-    // Devolver el tiempo bien formateado.
-    return `${minutos}:${parteEnSegundos}`;
-}
+	//Esta función coge segundos y lo formatea en minutos y segundos (por ejemplo, 80 -> 1:20)
+	formatoTiempo(segundos) {
+		// Minutos
+		var minutos = Math.floor(segundos/60);
+		// Segundos
+		var parteEnSegundos = segundos%60;
+		// Añadir ceros a la izquierda a los segundos
+		parteEnSegundos = parteEnSegundos.toString().padStart(2,'0');
+		// Devolver el tiempo bien formateado.
+		return `${minutos}:${parteEnSegundos}`;
+	}
+	
+};
