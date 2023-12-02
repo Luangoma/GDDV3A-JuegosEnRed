7e81b923470d876ccb2789c38691cad3e6ee7bf1
Sandro808 <ja.vazquez.2020@alumnos.urjc.es>
2023-12-02 22:27:29 +0100

    Añadido fondo y título a game_over
    Añadido un fondo animado a la escena "game_over". También se ha puesto el título.
    Falta por implementar el recuento de puntos.

--- Game/js/game.js
+++ Game/js/game.js
@@ -5,4 +5,5 @@ game.scene.add("map_test_1",map_test_1);
 game.scene.add("ui",ui);
 game.scene.add("game_over", game_over);
 game.scene.add("Credits", Credits);
-game.scene.start("MainMenu");
+//game.scene.start("MainMenu");
+game.scene.start("game_over");
