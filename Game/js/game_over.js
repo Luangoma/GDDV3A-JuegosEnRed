4afca1d8a4ae7a0360830b60921d1dbcb74f7a49
Sandro808 <ja.vazquez.2020@alumnos.urjc.es>
2023-12-03 13:14:32 +0100

    Convertido game_over a clase
    Se ha cambiado game_over para convertirlo en una clase que implemente Phaser.Scene.

--- Game/js/game_over.js
+++ Game/js/game_over.js
@@ -1,33 +1,45 @@
-var game_over = {
-	key: 'game_over',
-	preload: game_overPreload,
-	create: game_overCreate,
-	update: game_overUpdate
+class game_over extends Phaser.Scene 
+{
+	preload(){
+
+		this.load.spritesheet('town-on-fire', 'assets/town-on-fire-spritesheet.png', { frameWidth: 1200, frameHeight: 730 });
+	
+	}
+
+	create(){
+
+		var background = this.add.sprite(0,0, 'town-on-fire').setOrigin(0,0).setDisplaySize(config.width,config.height);
+	
+		this.anims.create({
+			key: 'animacionBackground',
+			frames: this.anims.generateFrameNumbers('town-on-fire', { start: 0, end: 64 }),
+			frameRate: 7,
+			repeat: -1
+		});
+	
+		background.anims.play("animacionBackground");
+	
+		var titulo = this.add.text(config.width/2, 150, 'Game Over', styleText_MedievalPixel_90).setOrigin(0.5);
+	
+	}
+
+	update(time, delta){
+
+	}
+
+	shutdown()
+	{
+		super.shutdown();
+	}
+
+	destroy()
+	{
+		super.destroy();
+	}
+
 };
 
-function game_overPreload(){
 
-	this.load.spritesheet('town-on-fire', 'assets/town-on-fire-spritesheet.png', { frameWidth: 1200, frameHeight: 730 });
 
-}
 
-function game_overCreate(){
 
-    var background = this.add.sprite(0,0, 'town-on-fire').setOrigin(0,0).setDisplaySize(config.width,config.height);
-
-	this.anims.create({
-		key: 'animacionBackground',
-		frames: this.anims.generateFrameNumbers('town-on-fire', { start: 0, end: 64 }),
-		frameRate: 7,
-		repeat: -1
-	});
-
-	background.anims.play("animacionBackground");
-
-	var titulo = this.add.text(config.width/2, 150, 'Game Over', styleText_MedievalPixel_90).setOrigin(0.5);
-
-}
-
-function game_overUpdate(time, delta){
-
-}
