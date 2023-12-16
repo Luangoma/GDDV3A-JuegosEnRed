32284dc62962ad8aaaa997920bea1598dc866eba
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-12-16 22:20:17 +0100

    Added working forum chat
    The clientside of the chat is now almost fully functional.
    
    It will display messages sent by the local user, and send them to the server.
    
    The user will update the messages list by getting the messages stored in the server.
    
    TODO: Add auto scroll when a new message is received and add proper user ID to sent messages.

--- Game/BurningVillageServer/src/main/resources/static/js/Forum.js
+++ Game/BurningVillageServer/src/main/resources/static/js/Forum.js
@@ -2,7 +2,10 @@ class ForumScene extends DragonScene
 {
 	menuBackground = {};
     botonSalir = {};
-	cajaTexto = {};
+	ForumChat = {};
+	messages_box_div = null;
+	
+	accumulated_time = 0;
 
     preload() {
         // Precarga del background con efecto de blur.
@@ -14,13 +17,14 @@ class ForumScene extends DragonScene
         // Añadir el background a la escena.
         this.menuBackground = this.add.image(0, 0, 'menuBackgroundBlurry').setOrigin(0, 0).setDisplaySize(config.width, config.height);
 		
-		
+		//reset the timer to 0:
+		this.accumulated_time = 0;
 		
 		const element = this.add.dom(config.width/2, config.height/2).createFromCache('cajaForo');
-		this.credentialsBox = element;
+		this.ForumChat = element;
 		
-		let messages_box_div = element.node.querySelector('#forum-messages-box');
-		console.log(messages_box_div);
+		this.messages_box_div = element.node.querySelector('#forum-messages-box');
+		console.log(this.messages_box_div);
 		
 		let that = this;
 		element.setPerspective(800);
@@ -54,7 +58,8 @@ class ForumScene extends DragonScene
 						}
 					}).done(function(data, textStatus, jqXHR) {
 						console.log("El mensaje se ha añadido satisfactoriamente al servidor.");
-						messages_box_div.innerHTML += that.getMessageString(localUser.user.username, mensaje);
+						that.addMessage(that.getMessageString(localUser.user.username, mensaje));
+						
 						
 					}).fail(function(data, textStatus, jqXHR) {
 						console.log("Error, no se ha añadido el mensaje al servidor.");
@@ -74,10 +79,50 @@ class ForumScene extends DragonScene
 		this.botonSalir.setCanBePressed(true);
     }
 	
+	update(time, delta)
+	{
+		let that = this;
+		let N_sec = 2; //makes the petitions every N seconds.
+		//if the accumulated wait time is greater than 1000 * N seconds, make a petition to get all of the new messages that were not on screen, just in case other users have sent messages.
+		if(this.accumulated_time > 1000 * N_sec){
+			$.ajax({
+				url: ip.http + '/posts',
+				method: 'GET',
+				processData: false,
+				headers: {
+					"Content-type": "application/json"
+				}
+			}).done(function(data, status, xhr){
+				console.log(data);
+				let full_messages_str = "";
+				
+				for(let i = 0; i < data.length; ++i){
+					full_messages_str += that.getMessageString("user", data[i].postContent);
+				}
+				console.log(full_messages_str);
+				that.setMessages(full_messages_str);
+				
+			}).fail(function(xhr, status, error){
+				//do nothing for now...
+			});
+			
+			this.accumulated_time = 0;
+		}
+		this.accumulated_time += delta;
+	}
+	
 	
 	getMessageString(name, msg){
 		let str = '<div class=\"message other-message\"><div><div class=\"name\">' + name + '</div><div class=\"text\">' + msg + '</div></div></div>';
 		return str;
+	}
+	
+	setMessages(message_string){
+		this.messages_box_div.innerHTML = message_string;
+	}
+	
+	addMessage(message_string){
+		this.messages_box_div.innerHTML += message_string;
 	}
 	
 }
