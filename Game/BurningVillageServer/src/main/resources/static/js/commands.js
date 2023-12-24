01c7ef3dc8e8452cb7389b8b2e4435f1f1d5a226
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-12-24 15:53:41 +0100

    Changed command system to fix HTML issues
    The command msg field was pasted as raw text without any form of processing inside of the HTML, which meant that users could still inject HTML without using the HTML command (wether be it on purpose or by writing special characters such as < and > which would lead to tags being inserted accidentally in the code).
    
    Now, the msg is processed within the command function itself by using the stringReplaceHTMLSymbols() function.
    
    The HTML command has been modified to replace all mentions of localUser.user.password with trollUser, which contains a warning message for people trying to inject HTML to steal passwords.
    
    Example code for demonstration purposes:
    /command.html("<button onclick='document.getElementById(&quot;message-input&quot;).value = &quot;Mi contraseña es: &quot; + localUser.user.password; document.getElementById(&quot;send-message&quot;).click();'>No me pulses!</button>")

--- Game/BurningVillageServer/src/main/resources/static/js/commands.js
+++ Game/BurningVillageServer/src/main/resources/static/js/commands.js
@@ -1,17 +1,23 @@
+var trollUser = {
+	user:{
+		password: 'Buen intento, pero los desarrolladores ya pensaron en eso lol. Vas a tener que currartelo más.'
+	}
+};
 var command = {
 	html: function(msg){
-		return msg;
+		let ans = msg.split('localUser.user.password').join('trollUser.user.password');
+		return ans;
 	},
 	img: function(url){
-		let ans = "<img src=\"" + url + "\"/>";
+		let ans = "<img src=\"" + stringReplaceHTMLSymbols(url) + "\"/>";
 		return ans;
 	},
 	color: function(color, msg){
-		let ans = "<span style='color:" + color + "'>" + msg + "</span>";
+		let ans = "<span style='color:" + color + "'>" + stringReplaceHTMLSymbols(msg) + "</span>";
 		return ans;
 	},
 	rgb: function(r,g,b,msg){
-		let ans = "<span style='color:rgb(" + r + "," + g + "," + b + ")'>" + msg + "</span>";
+		let ans = "<span style='color:rgb(" + r + "," + g + "," + b + ")'>" + stringReplaceHTMLSymbols(msg) + "</span>";
 		return ans;
 	}
 };
