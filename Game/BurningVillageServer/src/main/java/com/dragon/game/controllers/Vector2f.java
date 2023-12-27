be1e08770f3bb174f76f0636e48f8c16aa9792cc
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2023-12-27 22:29:05 +0100

    Created a Vector class for the server side
    The server now has a vector class that it can use to store positions and rotations for the players.
    
    This class can be modified to allow the server to perform more complex operations on the vectors if needed.

---
+++ Game/BurningVillageServer/src/main/java/com/dragon/game/controllers/Vector2f.java
@@ -0,0 +1,36 @@
+package com.dragon.game.controllers;
+
+public class Vector2f {
+	private float x,y;
+	
+	public Vector2f() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Vector2f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//Setters:
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	
+	//Getters:
+	public float getX() {
+		return this.x;
+	}
+	
+	public float getY() {
+		return this.y;
+	}
+	
+	
+}
