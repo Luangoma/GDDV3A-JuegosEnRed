c70aa5bb39f87aa8b19d89ec9c97c23798b8db89
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-01-01 23:28:41 +0100

    Changed font in game over screen
    The game over screen now displays certain texts with different fonts.
    
    This has been done to make the menu capable of displaying certain special characters that were not present in the previously chosen fonts, as well as making the menu look nicer with the better looking fonts that have been chosen.

--- Game/BurningVillageServer/src/main/resources/static/js/game_over.js
+++ Game/BurningVillageServer/src/main/resources/static/js/game_over.js
@@ -34,7 +34,7 @@ class game_over extends Phaser.Scene
 
 		// TÍTULO
 	
-		var titulo = this.add.text(config.width/2, 100, lang("key_game_over"), styleText_MedievalPixel_90).setOrigin(0.5);
+		var titulo = this.add.text(config.width/2, 80, lang("key_game_over"), styleText_AncientFont_90).setOrigin(0.5).setScale(1);
 
 
 		// PUNTUACIONES
@@ -44,15 +44,15 @@ class game_over extends Phaser.Scene
 		let xResultados = 200;
 		let yResultados = 150;
 
-		var tituloResultados = this.add.text(xResultados, yResultados, lang("key_results"), styleText_MedievalPixel_30).setOrigin(0);
+		var tituloResultados = this.add.text(config.width/2, yResultados + 10, lang("key_results"), styleText_AncientFont_90).setOrigin(0.5).setScale(0.7);
 		
-		var tituloJugador1 = this.add.text(xResultados, yResultados + separacionJugadores, this.player_1_data.name, styleText_MedievalPixel_30).setOrigin(0);
+		var tituloJugador1 = this.add.text(xResultados, yResultados + separacionJugadores, this.player_1_data.name, styleText_PixelSansSerif_30).setOrigin(0).setScale(0.7);
 
 		var casasQuemadasJugador1 = this.add.text(xResultados, yResultados + separacionJugadores*1 + separacionTextoGameOver*1, lang("key_burnt_houses") + ' : ' + this.player_1_data.score + " (+" + this.calcularPuntosCasasQuemadas(this.player_1_data.score) + ")", styleText_PixelSansSerif_18).setOrigin(0);
 
 		var totalPuntosJugador1 = this.add.text(xResultados, yResultados + separacionJugadores*1 + separacionTextoGameOver*2, lang("key_total") + " : " + this.calcularPuntosCasasQuemadas(this.player_1_data.score), styleText_PixelSansSerif_18).setOrigin(0);
 		
-		var tituloJugador2 = this.add.text(xResultados, yResultados + separacionJugadores*2 + separacionTextoGameOver*2, this.player_2_data.name, styleText_MedievalPixel_30).setOrigin(0);
+		var tituloJugador2 = this.add.text(xResultados, yResultados + separacionJugadores*2 + separacionTextoGameOver*2, this.player_2_data.name, styleText_PixelSansSerif_30).setOrigin(0).setScale(0.7);
 
 		var casasQuemadasJugador2 = this.add.text(xResultados, yResultados + separacionJugadores*2 + separacionTextoGameOver*3, lang("key_burnt_houses") + ' : ' + this.player_2_data.score + " (+" + this.calcularPuntosCasasQuemadas(this.player_2_data.score) + ")", styleText_PixelSansSerif_18).setOrigin(0);
 
